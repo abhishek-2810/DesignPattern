f50ef36922a9416cd2923b8e9ade5323ce1add8a
abhishek verma <28a.v10@gmail.com>
2023-12-21 22:49:58 +0530

    Add project files.

---
+++ DesignPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace DesignPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var singletonObjectFomClassA = Singleton.GetInstance;
+            singletonObjectFomClassA.PrintMessage("From Class A");
+
+            var singletonObjectFomClassB = Singleton.GetInstance;
+            singletonObjectFomClassB.PrintMessage("From Class B");
+        }
+    }
+}
---
+++ DesignPattern/Singleton.cs
@@ -0,0 +1,28 @@
+﻿namespace DesignPattern
+{
+    internal class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton? instance = null;
+
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine($"Object Count : {counter}");
+        }
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+
+                return instance;
+            }
+        }
+        public void PrintMessage(string message) => Console.WriteLine(message);
+    }
+}
