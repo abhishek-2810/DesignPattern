0629a1ebe293f03067643907827eff61bc8a8147
abhishek verma <28a.v10@gmail.com>
2023-12-21 23:55:43 +0530

    Add Thread Safety

--- DesignPattern/Singleton.cs
+++ DesignPattern/Singleton.cs
@@ -8,19 +8,31 @@
         // private variable to store singleton instance of class
         private static Singleton? instance = null;
 
+        private static readonly object lockObject = new object();
+
+        // Constructor should be private as we need to restrict instance creation (for other classes)
         private Singleton()
         {
             counter++;
             Console.WriteLine($"Object Count : {counter}");
         }
 
+        // One Property will be needed which will be responsible to provide class instance
+        // this is also static as if we'll not do that then we won't be able to access this property without static
         public static Singleton GetInstance
         {
             get
             {
                 if (instance == null)
                 {
-                    instance = new Singleton();
+                    // lock one thread and then create instance by then other thread will wait
+                    lock (lockObject)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Singleton();
+                        }
+                    }
                 }
 
                 return instance;
